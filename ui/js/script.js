0780b2bae6dad162fd2d547a5430ffc01203f77d
ph2160060 <ph2160060@transcosmos-ph.local>
2017-10-23 14:25:18 +0800

    80% done

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,3 +1,11 @@
 jQuery(function($){
-	
+	$('.select-header').click(function(){
+		var drop = $('.dropDown-radio'),
+				selectText = $(this).find('span');
+		drop.slideToggle('fast');
+		drop.find('.radio-title').click(function(){
+				selectText.text($(this).text());
+				drop.hide();
+		});
+	});
 });
